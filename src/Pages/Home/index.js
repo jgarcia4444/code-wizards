464faff380e2cefd1f95f797e3f448db22899a27
Jake Garcia <bballwiz4@gmail.com>
2024-03-28 13:29:06 -0600

    First commit

---
+++ src/Pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home = () => {
+
+    return (
+        <div className="">
+            <h1 className="text-4xl text-white font-bold">
+                Code Wizards
+            </h1>
+        </div>
+    )
+};
+
+export default Home;
