e180ac2b6eeaa7d85793ba9643717913f11dcbf2
Jake Garcia <bballwiz4@gmail.com>
2024-04-17 23:15:28 -0600

    Started to add in the logic to toggle which filter is selected.

--- src/Components/Portfolio/FilterProjects/Filter/index.js
+++ src/Components/Portfolio/FilterProjects/Filter/index.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { CiMonitor, CiMobile2 } from 'react-icons/ci';
 
 const Filter = ({text}) => {
 
+    const iconSize = 24;
+
+    const configureIcon = () => {
+        return text === "Web" ? <CiMonitor size={iconSize} color={"#000"} /> : <CiMobile2 size={iconSize} color={"#000"} />
+    }
+
     return (
-        <div className="text-white px-8 text-lg font-thin border-2 border-white rounded hover:cursor-pointer transition-all duration-300 hover:scale-105 hover:bg-white hover:text-black">{text}</div>
+        <div className=" px-6 text-lg  bg-white bg-opacity-10 rounded hover:cursor-pointer transition-all duration-300 hover:scale-105 hover:bg-opacity-30 active:scale-100 active:bg-opacity-10 flex flex-row items-center gap-1">
+            {configureIcon()}
+            {text}
+        </div>
     )
 }
 
-export default Filter;
+const mapStateToProps = state => {
+    return {
+        webSelected: state.portfolio.tabs.webSelected,
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Filter);
