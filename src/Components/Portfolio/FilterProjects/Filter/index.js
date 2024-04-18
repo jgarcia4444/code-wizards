f9a3501ccb131cb5e127cadc4688dfd67c48f71c
Jake Garcia <bballwiz4@gmail.com>
2024-04-18 11:47:28 -0600

    The filters view properties now change based off if the web selected property is picked.

--- src/Components/Portfolio/FilterProjects/Filter/index.js
+++ src/Components/Portfolio/FilterProjects/Filter/index.js
@@ -2,16 +2,34 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { CiMonitor, CiMobile2 } from 'react-icons/ci';
 
-const Filter = ({text}) => {
+const Filter = ({text, webTapped, mobileTapped, webSelected}) => {
 
     const iconSize = 24;
 
     const configureIcon = () => {
         return text === "Web" ? <CiMonitor size={iconSize} color={"#000"} /> : <CiMobile2 size={iconSize} color={"#000"} />
+    };
+
+    const handleClick = () => {
+        if (text === "Web") {
+            webTapped();
+        } else {
+            mobileTapped()
+        }
+    };
+
+    const dynamicViewProperties = () => {
+        if (webSelected === true && text === "Web") {
+            return "bg-opacity-100";
+        } else if (webSelected === false && text === "Mobile") {
+            return "bg-opacity-100";
+        } else {
+            return ""
+        }
     }
 
     return (
-        <div className=" px-6 text-lg  bg-white bg-opacity-10 rounded hover:cursor-pointer transition-all duration-300 hover:scale-105 hover:bg-opacity-30 active:scale-100 active:bg-opacity-10 flex flex-row items-center gap-1">
+        <div onClick={handleClick} className={`px-6 text-lg bg-white bg-opacity-10 rounded hover:cursor-pointer transition-all duration-300 hover:scale-105 hover:bg-opacity-30 active:scale-100 active:bg-opacity-10 flex flex-row items-center gap-1 ${dynamicViewProperties()}`}>
             {configureIcon()}
             {text}
         </div>
@@ -21,6 +39,13 @@ const Filter = ({text}) => {
 const mapStateToProps = state => {
     return {
         webSelected: state.portfolio.tabs.webSelected,
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        webTapped: () => dispatch({type: "WEB_SELECTED"}),
+        mobileTapped: () => dispatch({type: "MOBILE_SELECTED"}),
     }
 }
 
