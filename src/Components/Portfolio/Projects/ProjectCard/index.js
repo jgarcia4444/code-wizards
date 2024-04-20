38f1f72646737a0690f2f60ce9307e21e8611b89
Jake Garcia <bballwiz4@gmail.com>
2024-04-19 20:25:58 -0600

    The mobile apps in the portfolio now display the correct images.

--- src/Components/Portfolio/Projects/ProjectCard/index.js
+++ src/Components/Portfolio/Projects/ProjectCard/index.js
@@ -7,10 +7,10 @@ const ProjectCard = ({info}) => {
     const {title, subTitle, websiteUrl, img} = info;
 
     return (
-        <div className="relative w-80 h-48 rounded">
-            <img src={img} alt="" className="absolute top-0 left-0 w-full h-full rounded -z-10" />
+        <div className="relative rounded">
+            <img src={img} alt="" className=" w-full h-full rounded -z-10" />
             <a href={websiteUrl} target="_blank" className="">
-                <div className="w-full h-full bg-white bg-opacity-60 flex flex-row z-10 p-2 items-end transition-all duration-500 hover:bg-opacity-20 hover:cursor-pointer hover:scale-105 ">
+                <div className="absolute top-0 left-0 w-full h-full bg-white bg-opacity-60 flex flex-row z-10 p-2 items-end transition-all duration-500 hover:bg-opacity-0 hover:cursor-pointer hover:scale-105 hover:text-transparent">
                     <div className="flex flex-col">
                         <h4 className="text-xl font-bold font-serif">{title}</h4>
                         <h6 className="text-lg font-light">{subTitle}</h6>
