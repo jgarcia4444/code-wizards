f54cc186e6070b017d0017034de6cfd1cd4e071d
Jake Garcia <bballwiz4@gmail.com>
2024-04-21 22:07:22 -0600

    started to mess with the portfolio layout.

--- src/Components/Portfolio/Projects/ProjectCard/index.js
+++ src/Components/Portfolio/Projects/ProjectCard/index.js
@@ -7,8 +7,8 @@ const ProjectCard = ({info}) => {
     const {title, subTitle, websiteUrl, img} = info;
 
     return (
-        <div className="relative rounded">
-            <img src={img} alt="" className=" w-full h-full rounded -z-10" />
+        <div className="relative w-1/2 rounded">
+            <img src={img} alt="" className=" rounded -z-10" />
             <a href={websiteUrl} target="_blank" className="">
                 <div className="absolute top-0 left-0 w-full h-full bg-white bg-opacity-60 flex flex-row z-10 p-2 items-end transition-all duration-500 hover:bg-opacity-0 hover:cursor-pointer hover:scale-105 hover:text-transparent">
                     <div className="flex flex-col">
