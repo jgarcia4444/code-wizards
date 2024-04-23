edd26e2b386e516ec8128175d7a3cb3f90ad419f
Jake Garcia <bballwiz4@gmail.com>
2024-04-22 18:03:39 -0600

    obligatory commit

--- src/Components/Navbar/MobileNavLinks/index.js
+++ src/Components/Navbar/MobileNavLinks/index.js
@@ -18,12 +18,12 @@ const MobileNavLinks = ({links}) => {
     }
 
     return (
-        <div className="relative">
-            <div onClick={() => setAnimatedClassString("right-0 -z-10") } className="hover:cursor-pointer transition-all duration-300 hover:scale-105">
+        <div className="relative z-10 ">
+            <div onClick={() => setAnimatedClassString("right-0 opacity-100") } className="hover:cursor-pointer transition-all duration-300 hover:scale-105">
                 <CiMenuFries color={iconColor} size={iconSize} />
             </div>
-            <div className={`trainsition-all duration-700 absolute p-2 bg-black top-0 ${animatedClassString} z-10`}>
-                <div className="absolute -top-4 right-0 transition-all duration-300 hover:cursor-pointer hover:scale-110">
+            <div className={`trainsition-all duration-700 absolute bg-black top-0 ${animatedClassString} z-10 p-2`}>
+                <div className="absolute -top-6 right-0 transition-all duration-300 hover:cursor-pointer hover:scale-110">
                     <CiSquareRemove onClick={() => setAnimatedClassString(defaultClass)} size={iconSize} color={iconColor} />
                 </div>
                 {renderMobileNavRows()}
