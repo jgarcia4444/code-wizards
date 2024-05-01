c2a9364283711d91dbd431b6ef5056c208694dbb
Jake Garcia <bballwiz4@gmail.com>
2024-05-01 13:27:45 -0600

    removed the marketing service tab

--- src/redux/reducers/tabSelectionReducer/index.js
+++ src/redux/reducers/tabSelectionReducer/index.js
@@ -48,7 +48,7 @@ const serviceInfo = [
         {bulletPoint: "Monthly Reporting and Performance Analysis", details: "Providing detailed monthly reports and performance analysis to track progress, demonstrate ROI, and identify areas for improvement and optimization."},
     ]
 },
-    {icon: <CiMail color={iconColor} size={iconSize}/>, title: "Marketing", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."},
+    
 ];
 const initialState = {
     selectedIndex: 0,
